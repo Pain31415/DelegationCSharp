67279b0d9b5d1e656d35a8a91b3f17e973f0f31c
Pain31415 <Cherniy412@ukr.net>
2024-03-20 18:56:17 +0200

    Add project files.

---
+++ DelegationCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DelegationCSharp
+{
+    public delegate void DisplayMessageDelegate(string message);
+
+    public class MessageDisplayer
+    {
+        public static void DisplayMessageInConsole(string message)
+        {
+            Console.WriteLine("Programming is PAIN (Console): " + message);
+        }
+
+        public static void DisplayMessageInWindow(string message)
+        {
+            Console.WriteLine("Programming is PAIN (Window): " + message);
+        }
+
+        public static void DisplayMessageInLinux(string message)
+        {
+            Console.WriteLine("Programming is PAIN (Linux): " + message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMessageDelegate consoleDelegate = MessageDisplayer.DisplayMessageInConsole;
+            DisplayMessageDelegate windowDelegate = MessageDisplayer.DisplayMessageInWindow;
+            DisplayMessageDelegate linuxDelegate = MessageDisplayer.DisplayMessageInLinux;
+
+            consoleDelegate("Hello, this is displayed in the console.");
+            windowDelegate("Hello, this is displayed in the window.");
+            linuxDelegate("Hello, this is displayed in the Linux environment.");
+        }
+    }
+}
